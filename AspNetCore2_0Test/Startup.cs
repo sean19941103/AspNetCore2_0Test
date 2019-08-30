fad3cc7903bd86e854ac38d4b93bd17118076f04
seanl <seanl@DESKTOP-RO52VEU>
2019-08-30 15:43:17 +0800

    1

--- AspNetCore2_0Test/Startup.cs
+++ AspNetCore2_0Test/Startup.cs
@@ -23,7 +23,6 @@ namespace AspNetCore2_0Test
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-
             }
 
             app.Run(async (context) =>
